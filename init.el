319608bf656acadd4f5af1768add0c59761bf83a
mattiasdrp <mattias@kojin.tech>
2024-05-06 12:27:45 +0200

    Vertico posframe for @thriim

--- init.el
+++ init.el
@@ -19,37 +19,39 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Code:
 
-(setq elpaca-core-date '(20240227))
+(setq elpaca-core-date '(20240411))
 (defvar elpaca-installer-version 0.7)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-      :ref nil
-      :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-      :build (:not elpaca--activate-package)))
+			      :ref nil :depth 1
+			      :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+			      :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
- (build (expand-file-name "elpaca/" elpaca-builds-directory))
- (order (cdr elpaca-order))
- (default-directory repo))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
     (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
-  (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-     ((zerop (call-process "git" nil buffer t "clone"
-         (plist-get order :repo) repo)))
-     ((zerop (call-process "git" nil buffer t "checkout"
-         (or (plist-get order :ref) "--"))))
-     (emacs (concat invocation-directory invocation-name))
-     ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-         "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-     ((require 'elpaca))
-     ((elpaca-generate-autoloads "elpaca" repo)))
-      (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-    (error "%s" (with-current-buffer buffer (buffer-string))))
-((error) (warn "%s" err) (delete-directory repo 'recursive))))
+	(if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+		 ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+						 ,@(when-let ((depth (plist-get order :depth)))
+						     (list (format "--depth=%d" depth) "--no-single-branch"))
+						 ,(plist-get order :repo) ,repo))))
+		 ((zerop (call-process "git" nil buffer t "checkout"
+				       (or (plist-get order :ref) "--"))))
+		 (emacs (concat invocation-directory invocation-name))
+		 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+				       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+		 ((require 'elpaca))
+		 ((elpaca-generate-autoloads "elpaca" repo)))
+	    (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+	  (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
@@ -140,6 +142,12 @@ Similar to Vim's separation of command/insert modes"
   :group 'pokemacs-packages
   :type 'boolean
   :tag " Pandoc")
+
+(defcustom use-posframe nil
+  "If non-nil, uses the vertico-posframe packages."
+  :group 'pokemacs-packages
+  :type 'boolean
+  :tag "󰹉 Posframe")
 
 (defcustom use-spotify nil
   "If non-nil, uses the spotify packages."
@@ -1682,7 +1690,7 @@ in the current buffer.
   (lsp-lens-enable nil)
   (lsp-completion-provider :none)
   (lsp-completion-enable t)
-  (lsp-enable-imenu t)
+  (lsp-enable-imenu nil)
   (lsp-enable-snippet nil)
   (lsp-enable-dap-auto-configure nil)
   (lsp-disabled-clients '((python-mode . pyls)))
@@ -2190,7 +2198,14 @@ with a prefix ARG."
   (vertico-multiform-mode)
   (message "`vertico-multiform loaded"))
 
-(use-package vertico-posframe :defer t)
+(when use-posframe
+  (use-package vertico-posframe
+    :after vertico
+    :custom
+    (vertico-posframe-width 80)
+    :config
+    (vertico-posframe-mode 1)
+    (message "`vertico-posframe loaded")))
 
 (use-package consult
   :defer t
